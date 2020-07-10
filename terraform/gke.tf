11fcadd6cd4766a9709197688ab629b7cca17f51
Daniel Cortés <daniel.cortes@wizeline.com>
2020-07-10 12:04:53 -0500

    Change alias to URI

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -32,9 +32,13 @@ module "gke" {
 
   node_pools_oauth_scopes = {
     all = [
-      "gke-default",
-      "compute-rw",
-      "storage-rw"
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_write"
     ]
 
     default-node-pool = [
